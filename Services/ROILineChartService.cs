344f4daabcd350b06abcfc9df3f828abb822775e
DESKTOP <JP4>
2026-01-01 16:33:29 +0800

    update ROILineChartService

--- Services/ROILineChartService.cs
+++ Services/ROILineChartService.cs
@@ -1,14 +1,18 @@
-﻿using Stock_Online.DataAccess.SQLite.Interface;
+﻿using SqlKata;
+using Stock_Online.DataAccess.SQLite.Interface;
 using Stock_Online.Domain.Entities;
 using Stock_Online.DTOs;
 using Stock_Online.DTOs.Line_chart;
 using Stock_Online.Services.Interface;
+using System;
 
 namespace Stock_Online.Services
 {
     public class ROILineChartService : IROILineChartService
     {
         private readonly IStockDailyPriceRepository _repo;
+        private int _year;
+        private List<StockDailyPrice> _orderedDailyPrices;
 
         public ROILineChartService(IStockDailyPriceRepository repo)
         {
@@ -17,28 +21,39 @@ namespace Stock_Online.Services
         public async Task<List<LineSeriesDto>> GetChart(string stockId, int year, int days)
         {
             Console.WriteLine($"GetChart {stockId} {year} {days}");
-            List<StockDailyPrice> dailyPrices = await _repo.GetByStockIdAsync(stockId);
-            StockDailyPrice[] prices = dailyPrices.OrderByDescending(x => x.TradeDate).ToArray();
+            this._year = year;
 
-            //for (int i = 0; i < prices.Length; i++)
-            //{
-            //    Console.WriteLine($"{i}\t{prices[i].TradeDate} {prices[i].ClosePrice}");
-            //}
-            Console.WriteLine($"66: {prices[66].TradeDate}");
-            Console.WriteLine($"66-220: {prices[66+240].TradeDate}");
+            Query query = new Query("StockDailyPrice")
+                .Where("StockId", stockId);
 
-            var re = new List<LineSeriesDto>() { new LineSeriesDto()
-            {
-                Name = year.ToString(),
-                Points = dailyPrices.Where(x => x.TradeDate > new DateTime(year, 1, 1))
-                            .Select(x => new LinePointDto()
-                            {
-                                X = x.TradeDate.ToShortDateString(),
-                                Y = (double)x.ClosePrice
-                            }).ToList()
-            } };
+            List<StockDailyPrice> dailyPrices = await _repo.GetByQueryAsync(query);
+            //List<StockDailyPrice> dailyPrices = await _repo.GetByStockIdAsync(stockId);
+
+            _orderedDailyPrices = dailyPrices.OrderByDescending(x => x.TradeDate).ToList();
+
+            var lineSeriesDto = new List<LineSeriesDto>() { 
+                CreateLineSeries(days),
+                CreateLineSeries(days*2),
+                CreateLineSeries(days*3),
+                CreateLineSeries(days*5),
+                CreateLineSeries(days*10),
+            };
                 
-            return re;
+            return lineSeriesDto;
         }
+        private LineSeriesDto CreateLineSeries(int days)
+        {
+            return new LineSeriesDto()
+            {
+                Name = days.ToString(),
+                Points = _orderedDailyPrices.OrderBy(x => x.TradeDate).Where(x => x.TradeDate > new DateTime(_year, 1, 1))
+                    .Select(x => new LinePointDto()
+                    {
+                        X = x.TradeDate.ToShortDateString(),
+                        Y = (double)(x.ClosePrice / _orderedDailyPrices[_orderedDailyPrices.FindIndex(y => y.TradeDate == x.TradeDate) + days].ClosePrice)
+                    }).ToList()
+            };
+        }
+
     }
 }
